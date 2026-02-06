63976e27270df19e7dc113e2b71e867e1fc6fc67
“Matthew <Mterlinski@rrc.ca>
2026-02-06 15:34:59 -0600

    updated the app.ts , added the Health Check here to save complications of multiple controllers

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,22 @@
 import express, { Express } from 'express';
+import ticketRoutes from './api/v1/routes/ticketRoutes';
+import { HTTP_STATUS } from '././constants/httpStatuses';
 
 const app: Express = express();
-
 app.use(express.json());
+
+// Health Check Endpoint
+app.get('/api/v1/health', (req, res) => {
+  res.status(HTTP_STATUS.OK).json({
+    status: "OK",
+    uptime: process.uptime(),
+    timestamp: new Date().toISOString(),
+    version: "1.0.0"
+  });
+});
+
++
+// Routes
+app.use('/api/v1/tickets', ticketRoutes);
 
 export default app;
